3c01125a34df3dbfad10efb6cd361acc2edb7695
HP-5CG5387SWY\HP <sacoma@gmx.de>
2023-03-25 12:40:13 +0100

    Basis mit allen Dateien für das Programm erstellt

---
+++ Adressbuch/Classes/Contact.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressbuch.Classes
+{
+    public class Contact
+    {
+        private string _firstName;
+        private string _lastName;
+
+        private DateTime _birthday;
+        private string _street;
+        private string _postalCode;
+        private string _city;
+        private string _country;
+        private string _email;
+        private string _phone;
+
+        public string FullName { get { return _lastName + ", " + _firstName; } }
+        public string FirstName { get { return _firstName; } set { _firstName = value; } }
+        public string LastName { get { return _lastName; } set { _lastName = value; } }
+
+        public DateTime Birthday
+        {
+            get { return _birthday; }
+            set
+            {
+                _birthday = value;
+            }
+        }
+
+        public String Street
+        {
+            get { return _street; }
+            set
+            {
+                _street = value;
+            }
+        }
+
+        public string PostalCode
+        {
+            get { return _postalCode; }
+            set
+            {
+                _postalCode = value;
+            }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                _city = value;
+            }
+        }
+
+        public string Country { get { return _country; } set { _country = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+
+        public string Phone { get { return _phone; } set { _phone = value; } }
+    }
+}
---
+++ Adressbuch/ViewModels/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Adressbuch.Classes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adressbuch.ViewModels
+{
+    public class ContactViewModel
+    {
+        public ObservableCollection<Contact> ContaktListe { get; set; }
+
+        public ContactViewModel()
+        {
+            ContaktListe = new ObservableCollection<Contact>()
+            {
+                new Contact() { FirstName = "Peter", LastName="Lustig", Street = "Bauwagenstr. 1", PostalCode="98765", City="Löwenzahn-Stadt", Country="Deutschland", Phone="987654321", Email="peter@lustig.de", Birthday= DateTime.Now },
+                new Contact() { FirstName = "Willi", LastName="Willswissen", Street = "Wissengasse 2", PostalCode="78945", City="Wissens-Stadt", Country="Deutschland", Phone="456456456", Email="willi@willswissen.de", Birthday= DateTime.Now },
+                new Contact() { FirstName = "Marta", LastName="Machts", Street = "Macherstr. 3", PostalCode="45698", City="Macher-Stadt", Country="Deutschland", Phone="987654321", Email="matar@machts.de", Birthday= DateTime.Now }
+            };
+        }
+    }
+}
