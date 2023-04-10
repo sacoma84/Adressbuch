b3a4e2d4a1ecb0ed237d20c43291da050e70e71a
HP-5CG5387SWY\HP <sacoma@gmx.de>
2023-04-10 17:34:11 +0200

    DB-Verbindung codieren

--- Adressbuch/ViewModels/ContactViewModel.cs
+++ Adressbuch/ViewModels/ContactViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Adressbuch.Classes;
+using Adressbuch.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -14,13 +15,16 @@ namespace Adressbuch.ViewModels
 
         public ContactViewModel()
         {
-            
-            ContactList = new ObservableCollection<Contact>()
-            {
-                new Contact() { Id = 1, FirstName = "Peter", LastName="Lustig", Street = "Bauwagenstr. 1", PostalCode="98765", City="Löwenzahn-Stadt", Country="Deutschland", Phone="987654321", Email="peter@lustig.de", Birthday= new DateTime(2001,2,3) },
-                new Contact() { Id = 2, FirstName = "Willi", LastName="Willswissen", Street = "Wissengasse 2", PostalCode="78945", City="Wissens-Stadt", Country="Deutschland", Phone="456456456", Email="willi@willswissen.de", Birthday= new DateTime(2002,1,3) },
-                new Contact() { Id = 3, FirstName = "Marta", LastName="Machts", Street = "Macherstr. 3", PostalCode="45698", City="Macher-Stadt", Country="Deutschland", Phone="987654321", Email="matar@machts.de", Birthday= new DateTime(2003,2,1) }
-            };
+
+            DbModel dbModel = new DbModel();
+            ContactList = dbModel.ContactList;
+
+            //ContactList = new ObservableCollection<Contact>()
+            //{
+            //    new Contact() { Id = 1, FirstName = "Peter", LastName="Lustig", Street = "Bauwagenstr. 1", PostalCode="98765", City="Löwenzahn-Stadt", Country="Deutschland", Phone="987654321", Email="peter@lustig.de", Birthday= new DateTime(2001,2,3) },
+            //    new Contact() { Id = 2, FirstName = "Willi", LastName="Willswissen", Street = "Wissengasse 2", PostalCode="78945", City="Wissens-Stadt", Country="Deutschland", Phone="456456456", Email="willi@willswissen.de", Birthday= new DateTime(2002,1,3) },
+            //    new Contact() { Id = 3, FirstName = "Marta", LastName="Machts", Street = "Macherstr. 3", PostalCode="45698", City="Macher-Stadt", Country="Deutschland", Phone="987654321", Email="matar@machts.de", Birthday= new DateTime(2003,2,1) }
+            //};
         }
     }
 }
